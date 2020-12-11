c0b84fd6e82bdfc9f67081033d3a7dcbd83c5149
kinga977 <root@DESKTOP-06A06IP.localdomain>
2020-12-11 14:50:16 +1100

    wrote psuedo code in index

---
+++ index.rb
@@ -0,0 +1,4 @@
+# INPUTS: Menu choice, name, option for quit etc
+# DATA: Array of names
+# OUTPUT: size of class, Menu, names, prompts for quit
+# ADD PERSON IMPLEMENTATION: User input, add to array .push?
