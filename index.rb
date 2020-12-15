c04aac0f87d3cda5edbf2e1ed75349c5fe818cf1
kinga977 <root@DESKTOP-06A06IP.localdomain>
2020-12-15 12:35:14 +1100

    created a main_menu for the quiz

--- index.rb
+++ index.rb
@@ -1,4 +1,21 @@
-# INPUTS: Menu choice, name, option for quit etc
-# DATA: Array of names
-# OUTPUT: size of class, Menu, names, prompts for quit
-# ADD PERSON IMPLEMENTATION: User input, add to array .push?
+def main_menu
+    puts "Welcome to the quiz"
+    puts "short quiz[1]"
+    puts "medium quiz[2]"
+    puts "long quiz[3]"
+    puts "quit[4]"
+    input = gets.chomp
+    if input == "1"
+        puts "start quiz(1)"
+    elsif input == "2"
+        puts "start_quiz(2)"
+    elsif input == "3"
+        puts "start_quiz(3)"
+    elsif input == "4"
+        puts "quit"
+    else
+        puts "invalid input"
+    end
+end
+
+main_menu
